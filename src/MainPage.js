73f722ee641b3660914c436ebcb12286e2c1c68c
Hannah Ahmed <65818510+HannahAhmed@users.noreply.github.com>
2023-04-09 01:44:04 +0100

    Create MainPage.js

---
+++ src/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MainPage() {
+    return (
+      <div className="center">
+        <h1 className="title">Cryptocurrency Wallet</h1>
+        <h2 className="subtitle">Manage your cryptocurrencies with ease</h2>
+        <button>Create Account</button>
+        <button>Log In</button>
+      </div>
+    );
+  }
+  
+  export default MainPage;
