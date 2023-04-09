604f35a3e0108aa7f674c920bbe4c14402583a6f
ismail2754 <98770332+ismail2754@users.noreply.github.com>
2023-04-09 15:03:53 +0100

    testing buttons

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+function About() {
+  return (
+    <div>
+      <h1>About Cryptocurrency Wallet</h1>
+      <p>This is a cryptocurrency wallet app that lets you manage your cryptocurrencies with ease. It is built with React.</p>
+    </div>
+  );
+}
 
 function MainPage() {
-    return (
+  return (
+    <Router>
       <div className="center">
         <h1 className="title">Cryptocurrency Wallet</h1>
         <h2 className="subtitle">Manage your cryptocurrencies with ease</h2>
-        <button>Create Account</button>
-        <button>Log In</button>
+        <div>
+          <button>Create Account</button>
+          <button>Log In</button>
+          <Link to="/about"><button>About</button></Link>
+        </div>
       </div>
-    );
-  }
-  
-  export default MainPage;
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default MainPage;
