5ed7f336ecc82dfdb69942632a1caa8ac30395c2
Hannah Ahmed <65818510+HannahAhmed@users.noreply.github.com>
2023-04-09 01:43:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import MainPage from './MainPage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="center">
-          <h1 className="title">Cryptocurrency Wallet</h1>
-          <h2 className="subtitle">Manage your cryptocurrencies with ease</h2>
-          <button>Create Account</button>
-          <button>Log In</button>
-        </div>
+        <MainPage />
       </div>
     );
   }
